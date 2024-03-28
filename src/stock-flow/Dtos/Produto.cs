28431d891bc6c121bfa953f997e10ddab855b3f9
Iugor Sette Pereira <1395304@sga.pucminas.br>
2024-03-28 10:38:19 -0300

    implementacao de controler, model e dto para produto

---
+++ src/stock-flow/Dtos/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace stock_flow.Dtos
+{
+    public class Produto
+    {
+        [Required]
+        public string Id { get; set; } = string.Empty;
+
+        private Categoria? categoria;
+
+        [Required]
+        public string Nome { get; set; } = string.Empty;
+
+        [Required]
+        public string Descricao { get; set; } = string.Empty;
+
+        [Required]
+        Categoria Categoria { get; set; }
+
+        [Required]
+        public double PrecoCusto { get; set; }
+
+        [Required]
+        public double PrecoVenda { get; set; }
+
+        [Required]
+        public int Quantidade { get; set; }
+
+        public string Imagem { get; set; } = string.Empty;
+    }
+}
