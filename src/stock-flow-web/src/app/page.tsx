7538b2a136b74af471049df26aec42c28ed3538e
iugorsette <iugorsette@gmail.com>
2024-04-24 22:01:20 -0300

    projeto front end inicial

---
+++ src/stock-flow-web/src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-600">Stock Flow</h1>
+       <li><a 
+        href="https://tailwindcss.com/docs" 
+        className="text-blue-600 hover:text-blue-800"
+       > Tailwind</a></li>
+     </div>
+    
+  );
+}
