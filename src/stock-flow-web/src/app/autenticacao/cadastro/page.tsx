9dac72ee332295c31631292205e33f8a0a87262d
Gabriel Lourenço <gabriellourenco12ti@gmail.com>
2024-05-03 00:09:27 -0300

    melhoria na autenticacao

--- src/stock-flow-web/src/app/autenticacao/cadastro/page.tsx
+++ src/stock-flow-web/src/app/autenticacao/cadastro/page.tsx
@@ -3,19 +3,25 @@ import React, {useState} from "react";
 import {toast, ToastContainer} from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
-import {IAuthResponse, LoginUser} from "../services/autenticacao";
-import Home from "../page";
+import {IAuthResponse, RegisterUser} from "../../services/autenticacao";
 import Image from "next/image";
 import {LoadingSpinner} from "@/app/components/LoadingSpinner";
+import {useRouter} from "next/navigation";
 
-export default function Login() {
+export default function Cadastro() {
     const [isLoggedIn, setLoggedIn] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+    const [passwordConfirm, setPasswordConfirm] = useState("");
+    const [name, setName] = useState("");
+    const router = useRouter();
 
-    const makeLogin = async () => {
-        const res: IAuthResponse = await LoginUser({email: email, senha: password});
+    const makeRegister = async () => {
+        const res: IAuthResponse = await RegisterUser({
+            email: email, senha: password,
+            senhaConfirmada: passwordConfirm, nome: name
+        });
         console.log("res:", res);
         if (res.sucesso && res?.accessToken) {
             localStorage.setItem("accessToken", res.accessToken);
@@ -26,16 +32,16 @@ export default function Login() {
         }
     };
 
-    const handleLogin = async (e: React.FormEvent) => {
+    const handleRegister = async (e: React.FormEvent) => {
         e.preventDefault();
 
         setIsLoading(true);
-        await makeLogin();
+        await makeRegister();
         setIsLoading(false);
     };
 
     if (isLoggedIn) {
-        return <Home/>;
+        router.push("/#");
     }
 
     return (
@@ -54,7 +60,7 @@ export default function Login() {
             </ToastContainer>
 
             <section>
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="overflow-y-scroll flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <div className="flex items-center mb-6 text-2xl font-semibold text-indigo-900 dark:text-indigo-600">
                         <Image className="w-8 h-8 mr-2" src="/favicon.ico" alt="logo" width={80} height={80}/>
                         Stock Flow
@@ -63,9 +69,23 @@ export default function Login() {
                         className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Entre na sua conta
+                                Crie a sua conta
                             </h1>
-                            <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
+                            <form className="space-y-4 md:space-y-6" onSubmit={handleRegister}>
+                                <div>
+                                    <label htmlFor="name"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        Nome
+                                    </label>
+                                    <input type="text" name="name" id="name"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           placeholder="Nome completo"
+                                           value={name}
+                                           onChange={(element) => setName(element.target.value)}
+                                           required={true}
+                                           disabled={isLoading}
+                                    />
+                                </div>
                                 <div>
                                     <label htmlFor="email"
                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -93,15 +113,29 @@ export default function Login() {
                                            disabled={isLoading}
                                     />
                                 </div>
+                                <div>
+                                    <label htmlFor="passwordConfirm"
+                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                        Confirme a senha
+                                    </label>
+                                    <input type="password" name="passwordConfirm" id="passwordConfirm"
+                                           placeholder="••••••••"
+                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                           value={passwordConfirm}
+                                           onChange={(element) => setPasswordConfirm(element.target.value)}
+                                           required={true}
+                                           disabled={isLoading}
+                                    />
+                                </div>
                                 <button type="submit"
                                         className="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
                                         disabled={isLoading}>
-                                    {isLoading ? <LoadingSpinner/> : "Entrar"}
+                                    {isLoading ? <LoadingSpinner/> : "Criar conta"}
                                 </button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Não possui uma conta ainda? <a href="/cadastro"
-                                                                   className="font-medium text-indigo-600 hover:underline dark:text-indigo-500">
-                                    Criar uma conta</a>
+                                    Já possui uma conta? <a href="/autenticacao/login"
+                                                            className="font-medium text-indigo-600 hover:underline dark:text-indigo-500">
+                                    Entrar</a>
                                 </p>
                             </form>
                         </div>
--- src/stock-flow-web/src/app/autenticacao/login/page.tsx
+++ src/stock-flow-web/src/app/autenticacao/login/page.tsx
@@ -3,24 +3,21 @@ import React, {useState} from "react";
 import {toast, ToastContainer} from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
-import {IAuthResponse, RegisterUser} from "../services/autenticacao";
-import Home from "../page";
+import {IAuthResponse, LoginUser} from "../../services/autenticacao";
+import Home from "../../page";
 import Image from "next/image";
 import {LoadingSpinner} from "@/app/components/LoadingSpinner";
+import {useRouter} from "next/navigation";
 
-export default function Cadastrp() {
+export default function Login() {
     const [isLoggedIn, setLoggedIn] = useState(false);
-    const [isLoading, setIsLoading] = useState(false);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [passwordConfirm, setPasswordConfirm] = useState("");
-    const [name, setName] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const router = useRouter();
 
-    const makeRegister = async () => {
-        const res: IAuthResponse = await RegisterUser({
-            email: email, senha: password,
-            senhaConfirmada: passwordConfirm, nome: name
-        });
+    const makeLogin = async () => {
+        const res: IAuthResponse = await LoginUser({email: email, senha: password});
         console.log("res:", res);
         if (res.sucesso && res?.accessToken) {
             localStorage.setItem("accessToken", res.accessToken);
@@ -31,16 +28,16 @@ export default function Cadastrp() {
         }
     };
 
-    const handleRegister = async (e: React.FormEvent) => {
+    const handleLogin = async (e: React.FormEvent) => {
         e.preventDefault();
 
         setIsLoading(true);
-        await makeRegister();
+        await makeLogin();
         setIsLoading(false);
     };
 
     if (isLoggedIn) {
-        return <Home/>;
+        router.push("/#");
     }
 
     return (
@@ -59,7 +56,7 @@ export default function Cadastrp() {
             </ToastContainer>
 
             <section>
-                <div className="overflow-y-scroll flex flex-col items-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <div className="flex items-center mb-6 text-2xl font-semibold text-indigo-900 dark:text-indigo-600">
                         <Image className="w-8 h-8 mr-2" src="/favicon.ico" alt="logo" width={80} height={80}/>
                         Stock Flow
@@ -68,23 +65,9 @@ export default function Cadastrp() {
                         className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Crie a sua conta
+                                Entre na sua conta
                             </h1>
-                            <form className="space-y-4 md:space-y-6" onSubmit={handleRegister}>
-                                <div>
-                                    <label htmlFor="name"
-                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                        Nome
-                                    </label>
-                                    <input type="text" name="name" id="name"
-                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                           placeholder="Nome completo"
-                                           value={name}
-                                           onChange={(element) => setName(element.target.value)}
-                                           required={true}
-                                           disabled={isLoading}
-                                    />
-                                </div>
+                            <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
                                 <div>
                                     <label htmlFor="email"
                                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -112,29 +95,15 @@ export default function Cadastrp() {
                                            disabled={isLoading}
                                     />
                                 </div>
-                                <div>
-                                    <label htmlFor="passwordConfirm"
-                                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                        Confirme a senha
-                                    </label>
-                                    <input type="password" name="passwordConfirm" id="passwordConfirm"
-                                           placeholder="••••••••"
-                                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-600 focus:border-indigo-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                           value={passwordConfirm}
-                                           onChange={(element) => setPasswordConfirm(element.target.value)}
-                                           required={true}
-                                           disabled={isLoading}
-                                    />
-                                </div>
                                 <button type="submit"
                                         className="w-full text-white bg-indigo-600 hover:bg-indigo-700 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800"
                                         disabled={isLoading}>
-                                    {isLoading ? <LoadingSpinner/> : "Criar conta"}
+                                    {isLoading ? <LoadingSpinner/> : "Entrar"}
                                 </button>
                                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Já possui uma conta? <a href="/login"
-                                                            className="font-medium text-indigo-600 hover:underline dark:text-indigo-500">
-                                    Entrar</a>
+                                    Não possui uma conta ainda? <a href="/autenticacao/cadastro"
+                                                                   className="font-medium text-indigo-600 hover:underline dark:text-indigo-500">
+                                    Criar uma conta</a>
                                 </p>
                             </form>
                         </div>
