5060b9f494660662898e58100b058d1069422161
Gabriel Lourenço <gabriellourenco12ti@gmail.com>
2024-05-04 13:05:51 -0300

    ajuste de deploy

--- src/stock-flow-web/src/app/page.tsx
+++ src/stock-flow-web/src/app/page.tsx
@@ -1,10 +1,24 @@
 'use client'
+import { useEffect, useState } from 'react';
 import Navigation from "./components/Navigation";
 import Login from "./autenticacao/login/page";
+import {Loading} from "@/app/components/Loading";
+import {useRouter} from "next/navigation";
 
 export default function Home() {
-    const isBrowser = typeof window !== 'undefined';
-    const accessToken = isBrowser ? localStorage.getItem("accessToken") : null;
+    const [accessToken, setAccessToken] = useState<string | null>(null);
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        const token = localStorage.getItem("accessToken");
+        setAccessToken(token);
+        setLoading(false);
+    }, []);
+
+    if (loading) {
+        return <Loading/>;
+    }
 
     return accessToken ? (
         <div>
@@ -14,7 +28,5 @@ export default function Home() {
 
             <Navigation/>
         </div>
-    ) : (
-        <Login/>
-    );
+    ) : router.push("autenticacao/login")
 }
