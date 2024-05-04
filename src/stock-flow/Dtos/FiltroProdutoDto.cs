fa9a0287b9e29b36450a51b955eb6550e45f7669
Marcelo ferreira <marcelo.hf2730@outlook.com>
2024-05-04 11:59:05 -0300

    Feature: filtro busca fornecedor

--- src/stock-flow/Dtos/FiltroProdutoDto.cs
+++ src/stock-flow/Dtos/FiltroProdutoDto.cs
@@ -1,16 +1,9 @@
 ﻿namespace stock_flow.Dtos
 {
-    public class FiltroProdutoDto
+    public class FiltroFornecedorDto
     {
         public string? Nome { get; set; }
-        public string? Categoria { get; set; }
-        public string? Quantidade { get; set; }
-        public string? QuantidadeMinima { get; set; }
-        public string? QuantidadeMaxima { get; set; }
-        public string? PrecoCustoMinimo { get; set; }
-        public string? PrecoCustoMaximo { get; set; }
-        public string? PrecoVendaMinimo { get; set; }
-        public string? PrecoVendaMaximo { get; set; }
-        public string? Fornecedor { get; set; }
+        public string? Contato { get; set; }
+        public string? Endereco { get; set; }
     }
 }
