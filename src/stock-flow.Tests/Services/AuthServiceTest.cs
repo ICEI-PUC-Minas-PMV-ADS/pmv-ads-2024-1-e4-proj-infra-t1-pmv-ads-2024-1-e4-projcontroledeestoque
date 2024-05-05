37caafe1e9729369ba28e97dc91b9fe47d422175
Gabriel Lourenço <gabriellourenco12ti@gmail.com>
2024-05-05 01:22:26 -0300

    testes do auth service

--- src/stock-flow.Tests/Services/AuthServiceTest.cs
+++ src/stock-flow.Tests/Services/AuthServiceTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Moq;
 using NUnit.Framework;
@@ -17,7 +18,9 @@ public class AuthServiceTests
     public void SetUp()
     {
         var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
-        _mockUserManager = new Mock<UserManager<ApplicationUser>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
+        _mockUserManager =
+            new Mock<UserManager<ApplicationUser>>(userStoreMock.Object, null, null, null, null, null, null, null,
+                null);
 
         var roleStoreMock = new Mock<IRoleStore<ApplicationRole>>();
         _mockRoleManager = new Mock<RoleManager<ApplicationRole>>(roleStoreMock.Object, null, null, null, null);
@@ -61,5 +64,44 @@ public class AuthServiceTests
 
         // Assert
         _mockUserManager.Verify(um => um.CreateAsync(user, "password"), Times.Once);
+    }
+
+    [Test]
+    public async Task LoginAsync_ValidCredentials_ReturnsLoginDto()
+    {
+        // Arrange
+        var user = new ApplicationUser { Email = "test@test.com", PasswordHash = "password" };
+        _mockUserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(user);
+        _mockUserManager.Setup(x => x.CheckPasswordAsync(user, It.IsAny<string>())).ReturnsAsync(true);
+        _mockUserManager.Setup(x => x.GetRolesAsync(user)).ReturnsAsync(new List<string>());
+
+        // Act
+        var result = await _service.LoginAsync("test@test.com", "password");
+
+        // Assert
+        Assert.That(result, !Is.Null);
+        Assert.That("test@test.com", Is.EqualTo(result.Email));
+    }
+
+    [Test]
+    public void LoginAsync_InvalidEmail_ThrowsAuthException()
+    {
+        // Arrange
+        _mockUserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync((ApplicationUser)null);
+
+        // Act & Assert
+        Assert.ThrowsAsync<AuthException>(() => _service.LoginAsync("invalid@test.com", "password"));
+    }
+
+    [Test]
+    public void LoginAsync_InvalidPassword_ThrowsAuthException()
+    {
+        // Arrange
+        var user = new ApplicationUser { Email = "test@test.com", PasswordHash = "password" };
+        _mockUserManager.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(user);
+        _mockUserManager.Setup(x => x.CheckPasswordAsync(user, It.IsAny<string>())).ReturnsAsync(false);
+
+        // Act & Assert
+        Assert.ThrowsAsync<AuthException>(() => _service.LoginAsync("test@test.com", "invalidpassword"));
     }
 }
