b41d278a30d6c3b8fabcd2683cdf803047953a29
Iugor Sette Pereira <iugorsette@gmail.com>
2024-05-20 11:27:37 -0300

    refactor: implements a modalReport and report view

---
+++ src/stock-flow-web/src/app/relatorios/page.tsx
@@ -0,0 +1,214 @@
+'use client'
+import 'react-toastify/dist/ReactToastify.css'
+import { useEffect, useState, useRef } from 'react'
+import Price from '../components/Price'
+import Navigation from '../components/Navigation'
+import { NotePencil, TrashSimple } from '@phosphor-icons/react'
+import DeleteModal from '../components/DeleteModal'
+import ProductModal from '../components/ProductModal'
+import { ToastContainer, toast } from 'react-toastify'
+import EditProductModal from '../components/EditProductModal'
+import ProductDetailsModal from '../components/ProductDetailsModal'
+import { useRouter } from 'next/navigation'
+import { getAccessToken } from '@/app/utils/acess-token'
+import { Loading } from '@/app/components/Loading'
+import { URLS } from '@/app/utils/constantes'
+import { Movimentacao, getMovimentacoes } from '../services/movimentacoes'
+import MovimentacaoModal from '../components/MovimentacaoModal'
+
+export default function Relatorios() {
+  const [accessToken, setAccessToken] = useState<string | null>(null)
+  const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState<Movimentacao[]>([])
+  const [filter, setFilter] = useState('')
+  const [deleteModal, setDeleteModal] = useState(false)
+  const [product, setProduct] = useState<Movimentacao | null>(null)
+  const [createModal, setCreateModal] = useState(false)
+  const [editModal, setEditModal] = useState(false)
+  const [detailsModalOpen, setDetailsModalOpen] = useState(false)
+  const [selectedProduct, setSelectedProduct] = useState<Movimentacao | null>(
+    null
+  )
+  const router = useRouter()
+  const formRef = useRef(null)
+
+  const updateMovimentacoes = (message?: string) => {
+    getMovimentacoes().then((data) => {
+      setProducts(data)
+      message && toast.success(message)
+    })
+  }
+
+  const handleCloseCreateModal = () => {
+    setCreateModal(!createModal)
+    updateMovimentacoes()
+    setTimeout(() => toast.success(`Movimentação criado com sucesso!`), 1000)
+  }
+
+  const handleOpenCreateModal = () => {
+    setCreateModal(!createModal)
+  }
+
+
+
+  const handleOpenDetailsModal = (product: Movimentacao) => {
+    setSelectedProduct(product)
+    setDetailsModalOpen(true)
+  }
+
+  const handleCloseDetailsModal = () => {
+    setDetailsModalOpen(false)
+    setSelectedProduct(null)
+  }
+
+  const handleSearch = () => {
+    getMovimentacoes({ name: filter }).then((data) => {
+      setProducts(data)
+    })
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    handleSearch()
+  }
+
+  useEffect(() => {
+    const accessToken = getAccessToken()
+    setAccessToken(accessToken)
+    setLoading(false)
+  }, [])
+
+  useEffect(() => {
+    updateMovimentacoes()
+  }, [filter])
+
+  if (loading) {
+    return <Loading />
+  }
+
+  if (!accessToken) {
+    router.push(URLS.AUTENTICACAO_PATH + URLS.LOGIN_PATH)
+    return
+  }
+
+  return createModal ? (
+    <MovimentacaoModal handleCloseCreateModal={handleCloseCreateModal} />
+  ) : (
+    <div>
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <div>
+        <h1 className='text-indigo-600'>
+          Stock Flow <span className='text-gray-500'>Movimentações</span>
+        </h1>
+        <div className='flex w-full justify-between'>
+          <Navigation />
+          <button
+            onClick={handleOpenCreateModal}
+            className='middle none text-zinc-950 center mr-4 rounded-lg bg-amber-600 py-3 px-6 font-sans text-xs font-bold uppercase text-white shadow-md shadow-amber-500/20 transition-all hover:shadow-lg hover:shadow-amber-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
+            data-ripple-light='true'>
+            Nova Movimentação
+          </button>
+        </div>
+      </div>
+
+      <div className='mt-8 max-w-2xl mx-auto mb-6'>
+        <form ref={formRef} onSubmit={handleSubmit}>
+          <label className='mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300'>
+            Search
+          </label>
+          <div className='relative'>
+            <div className='flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none'>
+              <svg
+                className='w-5 h-5 text-gray-500 dark:text-gray-400'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'></path>
+              </svg>
+            </div>
+            <input
+              type='search'
+              id='default-search'
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+              className='block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-indigo-500 dark:focus:border-indigo-500'
+              placeholder='Notebook, Fones, etc.'
+              required></input>
+            <button
+              type='submit'
+              className='text-white absolute right-2.5 bottom-2.5 bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 dark:focus:ring-indigo-800'>
+              Pesquisar
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <table className='w-full'>
+        <thead className='bg-gray-900'>
+          <tr>
+            <th className='py-1 px-4 max-w-prose'>Qtd</th>
+            <th className='py-1 px-4 max-w-prose'>Produto</th>
+            <th className='py-1 px-4 max-w-prose'>Tipo</th>
+            <th className='py-1 px-4 max-w-prose'>Forncedores</th>
+            <th className='py-1 px-4 max-w-prose'>Valor total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index) => (
+            <tr
+              className={`bg-gray-${index % 2 === 0 ? '950' : '900'} py-2`}
+              key={product.id}>
+              <td className='py-1 px-4 max-w-prose '>{product.quantidade}</td>
+              <td
+                className={`py-1 px-4 max-w-prose border-s border-gray-${
+                  index % 2 === 0 ? '900' : '950'
+                }`}>
+                {product.produtoNome}
+              </td>
+              <td
+                className={`py-1 px-4 max-w-prose border-s border-gray-${
+                  index % 2 === 0 ? '900' : '950'
+                }`}>
+                {product.tipo}
+              </td>
+              <td className={`py-1 px-4 max-w-prose border-s border-gray-950`}>
+                {product.fornecedoresNomes
+                  .map((e) => (e.length > 0 ? e : ''))
+                  .join(`, `)}
+              </td>
+
+              <td
+                className={`py-1 px-4 max-w-prose border-s border-gray-${
+                  index % 2 === 0 ? '900' : '950'
+                }`}>
+                <Price value={product.valor}></Price>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {detailsModalOpen && selectedProduct && (
+        // <ProductDetailsModal
+        //   product={selectedProduct}
+        //   handleCloseDetailsModal={handleCloseDetailsModal}
+        // />
+        <></>
+      )}
+    </div>
+  )
+}
---
+++ src/stock-flow-web/src/app/services/movimentacoes.ts
@@ -0,0 +1,62 @@
+import axios from 'axios'
+
+export type Movimentacao = {
+  id: string
+  produtoNome: string
+  fornecedoresNomes: string[]
+  tipo: 'Compra' | 'Venda' 
+  quantidade: number
+  valor: number
+  usuario: string
+  data: Date
+}
+
+const axiosInstance = axios.create({
+  baseURL: 'https://stock-flow.azurewebsites.net/api/v1/relatorios/movimentacoes',
+  timeout: 30000,
+})
+
+export async function getMovimentacoes(query?: any): Promise<Movimentacao[]> {
+  const queryString = query?.name ? `?nome=${query.name}` : ''
+  try {
+    const response = await axiosInstance.get(queryString)
+    return response.data
+  } catch (error) {
+    console.error('Erro ao obter movimentacao:', error)
+    throw error
+  }
+}
+
+export async function createMovimentacao(
+  fornecedor: Movimentacao
+): Promise<Movimentacao> {
+  try {
+    const response = await axiosInstance.post('', fornecedor)
+    return response.data
+  } catch (error) {
+    console.error('Erro ao criar movimentacao:', error)
+    throw error
+  }
+}
+
+export async function deleteFornecedor(id: string): Promise<Movimentacao> {
+  try {
+    const response = await axiosInstance.delete(`/${id}`)
+    return response.data
+  } catch (error) {
+    console.error('Erro ao excluir movimentacao:', error)
+    throw error
+  }
+}
+
+export async function updateFornecedor(
+  movimentacao: Movimentacao
+): Promise<Movimentacao> {
+  try {
+    const response = await axiosInstance.put(`/${movimentacao.id}`, movimentacao)
+    return response.data
+  } catch (error) {
+    console.error('Erro ao atualizar movimentacao:', error)
+    throw error
+  }
+}
